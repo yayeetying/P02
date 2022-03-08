962aa8c3112daf9898e488d6f1caf7f30aa6dda0
Joshua Kloepfer <jkloepfer20@stuy.edu>
2022-03-08 08:20:19 -0500

    work

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,12 @@ import urllib
 import random
 
 app = Flask(__name__)
-app.secret_key = urandom(32)
+#app.secret_key = urandom(32)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
