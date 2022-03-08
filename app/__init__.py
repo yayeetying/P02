d2fd8e8b1e28ea02d82eab9f7d28b74c0510ec1d
Yaying <yliangli20@stuy.edu>
2022-03-08 06:44:40 -0500

    initial app

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, session, redirect
+import sqlite3, os.path
+import json
+import urllib
+import random
+
+app = Flask(__name__)
+app.secret_key = urandom(32)
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
